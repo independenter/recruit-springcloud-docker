ab3f555f18b4aad34d21789e9136a27677bd5618
independenter <2531256442@qq.com>
2019-05-21 19:26:02 +0800

    加入ribbon

---
+++ service-ribbon/src/main/java/com/springcloud/serviceribbon/HelloService.java
@@ -0,0 +1,19 @@
+package com.springcloud.serviceribbon;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class HelloService {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    public String hiService(String name) {
+        return restTemplate.getForObject("http://EUREKA-CLIENT/hi?name="+name,String.class);
+    }
+
+
+}
---
+++ service-ribbon/src/main/java/com/springcloud/serviceribbon/ServiceRibbonApplication.java
@@ -0,0 +1,30 @@
+package com.springcloud.serviceribbon;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@Slf4j
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class ServiceRibbonApplication {
+
+    public static void main(String[] args) {
+        logger.info("ServiceRibbonApplication Starting ... ");
+        SpringApplication.run(ServiceRibbonApplication.class, args);
+        logger.info("ServiceRibbonApplication Started ... ");
+    }
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+}
