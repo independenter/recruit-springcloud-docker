3ccd98a901a3cc55d21b4bf861187ebaca714982
independenter <2531256442@qq.com>
2019-05-21 21:38:10 +0800

    整合hystrix功能

--- service-ribbon/src/main/java/com/springcloud/serviceribbon/HelloService.java
+++ service-ribbon/src/main/java/com/springcloud/serviceribbon/HelloService.java
@@ -1,7 +1,7 @@
 package com.springcloud.serviceribbon;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,9 +11,12 @@ public class HelloService {
     @Autowired
     RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "hiError")
     public String hiService(String name) {
         return restTemplate.getForObject("http://EUREKA-CLIENT/hi?name="+name,String.class);
     }
 
-
+    public String hiError(String name) {
+        return "hi,"+name+",sorry,error!";
+    }
 }
