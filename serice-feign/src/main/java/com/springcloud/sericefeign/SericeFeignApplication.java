e2bb06628265a28c40f7c3d7d83ba56b1eb90ad0
independenter <2531256442@qq.com>
2019-05-21 20:52:50 +0800

    整合feign完毕

--- serice-feign/src/main/java/com/springcloud/sericefeign/SericeFeignApplication.java
+++ serice-feign/src/main/java/com/springcloud/sericefeign/SericeFeignApplication.java
@@ -1,13 +1,23 @@
 package com.springcloud.sericefeign;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableFeignClients
+@Slf4j
 public class SericeFeignApplication {
 
     public static void main(String[] args) {
+        logger.info("ServiceFeignApplication Starting ... ");
         SpringApplication.run(SericeFeignApplication.class, args);
+        logger.info("ServiceFeignApplication Starting ... ");
     }
 
 }
