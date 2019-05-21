17433e2a487b92d392d387437c7c2e19c7616e21
independenter <2531256442@qq.com>
2019-05-21 19:26:33 +0800

    加入feign

---
+++ serice-feign/src/main/java/com/springcloud/sericefeign/SericeFeignApplication.java
@@ -0,0 +1,13 @@
+package com.springcloud.sericefeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SericeFeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SericeFeignApplication.class, args);
+    }
+
+}
