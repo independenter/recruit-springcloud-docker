1279e9411a8c1bfdd4c90f550789dacb1c7c0b8e
independenter <2531256442@qq.com>
2019-05-23 15:05:53 +0800

    gateway

---
+++ gs-gateway/src/main/java/com/sringcloud/gsgateway/GsGatewayApplication.java
@@ -0,0 +1,62 @@
+package com.sringcloud.gsgateway;
+
+import lombok.Data;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+// tag::code[]
+@SpringBootApplication
+@EnableConfigurationProperties(UriConfiguration.class)
+@RestController
+public class GsGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GsGatewayApplication.class, args);
+    }
+
+    // tag::route-locator[]
+    ////curl --dump-header - --header 'Host: www.hystrix.com' http://localhost:8080/delay/3
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder, UriConfiguration uriConfiguration) {
+        String httpUri = uriConfiguration.getHttpbin();
+        return builder.routes()
+                .route(p -> p
+                        .path("/get")
+                        .filters(f -> f.addRequestHeader("Hello", "World"))
+                        .uri(httpUri))
+                .route(p -> p
+                        .host("*.hystrix.com")
+                        .filters(f -> f
+                                .hystrix(config -> config
+                                        .setName("mycmd")
+                                        .setFallbackUri("forward:/fallback")))
+                        .uri(httpUri))
+                .build();
+    }
+    // end::route-locator[]
+
+
+    // tag::fallback[]
+    @RequestMapping("/fallback")
+    public Mono<String> fallback() {
+        return Mono.just("fallback");
+    }
+    // end::fallback[]
+}
+// tag::uri-configuration[]
+@ConfigurationProperties
+@Data
+class UriConfiguration {
+
+    private String httpbin="http://httpbin.org";
+}
+// end::uri-configuration[]
+// end::code[]
